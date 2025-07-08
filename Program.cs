b8162177782464b4a388c8b1679e1968db09923e
arghyadharmsft <117205543+arghyadharmsft@users.noreply.github.com>
2025-07-08 19:46:45 +0000

    Adding AZURE_SQL_CONNECTIONSTRING

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ else
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
     //options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-        options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+        options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
     // builder.Services.AddStackExchangeRedisCache(options =>
     // {
     // options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
